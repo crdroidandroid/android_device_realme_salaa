03e5f213c51354024d52355b9a0456cd7998457e
Giovanni Ricca <giovanniricca@proton.me>
2025-06-11 09:46:01 +0530

    salaa: Enable ELF checks for libimsma
    Rename libsink library to not conflict with the rust module
    
    Change-Id: I16028606d42d63525b5ef3130d86279bf35e26e1

--- extract-files.sh
+++ extract-files.sh
@@ -51,6 +51,10 @@ done
 
 function blob_fixup {
     case "$1" in
+        system_ext/lib64/libimsma.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "libsink.so" "libsink-mtk.so" "${2}"
+            ;;
         system_ext/lib64/libsource.so)
             grep -q libui_shim.so "$2" || "$PATCHELF" --add-needed libui_shim.so "$2"
             ;;
