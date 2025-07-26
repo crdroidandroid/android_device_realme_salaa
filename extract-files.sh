3620335c89a32b6d035571f3387c4ab91ea527d9
Debayan Kar <kardebayan3@gmail.com>
2025-07-26 15:49:08 +0000

    salaa: Patch MTK Neural Networks with libbase_shim
    Fixes :-
    
    - F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.neuralnetworks@1.3-service-mtk-neuron": cannot locate symbol "_ZN7android4base4TrimERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE" referenced by "/vendor/bin/hw/android.hardware.neuralnetworks@1.3-service-mtk-neuron"...
    
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -78,7 +78,7 @@ function blob_fixup {
         vendor/lib/hw/audio.primary.mt6785.so)
              "${PATCHELF}" --replace-needed "libalsautils.so" "libalsautils-v31.so" "${2}"
              ;;
-        odm/bin/hw/vendor.oplus.hardware.charger@1.0-service|vendor/lib*/libnvram.so|vendor/lib*/libsysenv.so)
+        odm/bin/hw/vendor.oplus.hardware.charger@1.0-service|vendor/bin/hw/android.hardware.neuralnetworks@1.3-service-mtk-neuron|vendor/lib*/libnvram.so|vendor/lib*/libsysenv.so)
             [ "$2" = "" ] && return 0
             grep -q "libbase_shim.so" "${2}" || "${PATCHELF}" --add-needed "libbase_shim.so" "${2}"
             ;;
